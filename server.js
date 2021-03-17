eb9c0536db5bef37983e6ddb28aa0abe46ef9099
shahrour <ahmadshahrour951@gmail.com>
2021-03-17 20:50:09 +0400

    Setup

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv-safe').config();
 const express = require('express');
 const exphbs = require('express-handlebars');
+
 const morgan = require('morgan');
 
 const db = require('./models');
+const routes = require('./routes');
+
+const Handlebars = require('handlebars');
+const {
+  library,
+  dom,
+  icon,
+} = require('@fortawesome/fontawesome-svg-core')
+const fas = require('@fortawesome/free-solid-svg-icons').fas;
+const fab = require('@fortawesome/free-brands-svg-icons').fab;
+
+library.add(fas);
+library.add(fab);
+
+Handlebars.registerHelper('fontawesome-css', function () {
+  return new Handlebars.SafeString(dom.css());
+});
+Handlebars.registerHelper('fontawesome-icon', function (args) {
+  return new Handlebars.SafeString(
+    icon({ prefix: 'fas', iconName: args.hash.icon }).html
+  );
+});
+
+// Handlebars.registerHelper('fontawesome-icon', function (args) {
+//   return new Handlebars.SafeString(
+//     icon({ prefix: 'fab', iconName: args.hash.icon }).html
+//   );
+// });
+
+
 
 const app = express();
 
@@ -19,10 +50,12 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 app.use(morgan('combined'));
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.render('home');
-});
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use('/', routes);
 
 const PORT = process.env.PORT || 8000;
 
