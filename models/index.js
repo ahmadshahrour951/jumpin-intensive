e1b25173bfa7978a855a64cd26eaa0683a23524e
shahrour <ahmadshahrour951@gmail.com>
2021-03-17 22:29:48 +0400

    user model

--- models/index.js
+++ models/index.js
@@ -22,11 +22,21 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+db.users = require('./user.model')(sequelize, Sequelize);
 db.games = require('./game.model')(sequelize, Sequelize);
+
 /////////////////////////////////////////////////////////////////////////////////////////
 // Relationships
 /////////////////////////////////////////////////////////////////////////////////////////
+db.users.hasMany(db.games);
+db.games.belongsTo(db.users);
 
+db.users.belongsToMany(db.games, {
+  through: 'user_games',
+});
+db.games.belongsToMany(db.users, {
+  through: 'user_games',
+});
 
 /////////////////////////////////////////////////////////////////////////////////////////
 // Test database connection
---
+++ models/user.model.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'user',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      underscored: true,
+    }
+  );
+
+  return User;
