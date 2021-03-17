873425fd33fae3240ea035d7a22ac2d6ede083ef
shahrour <ahmadshahrour951@gmail.com>
2021-03-17 21:06:41 +0400

    Create Game

--- controllers/game.controller.js
+++ controllers/game.controller.js
@@ -1,8 +1,23 @@
 const moment = require('moment');
 const db = require('../models');
 const gameController = {
+  createGame,
   findGames,
 };
+
+function createGame(req, res, next) {
+  if (req.method === 'GET') {
+    return res.render('game-create');
+  }
+  req.body.starts_at = new Date(req.body.starts_at).toISOString();
+  req.body.ends_at = new Date(req.body.ends_at).toISOString();
+
+  db.games
+    .create(req.body)
+    .then(() => res.redirect('/games'))
+    .catch((err) => console.log(err));
+}
+
 function findGames(req, res, next) {
   db.games
     .findAll({ raw: true })
