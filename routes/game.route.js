0d974cba8908267d655991b535abc8ad358d94ab
shahrour <ahmadshahrour951@gmail.com>
2021-03-17 21:08:17 +0400

    Update game.route.js

--- routes/game.route.js
+++ routes/game.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const gameController = require('../controllers/game.controller');
 
 router.use('/new', gameController.createGame);
+router.get('/', gameController.findGames);
 router.get('/:gameId', gameController.findGame);
 router.post('/:gameId', gameController.updateGame);
 router.delete('/:gameId', gameController.deleteGame);
